3f4059c9337a53847d4ecc290ad0105ba7122919
IronBaby <qstoretto@sina.com>
2020-09-13 00:13:18 +0800

    替换1.两数之和.cpp

--- Week_01/homework/1.两数之和.cpp
+++ Week_01/homework/1.两数之和.cpp
@@ -8,7 +8,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        int sumBL = 0;
+        for(int i = 0;i < nums.size() ;i++){
+            for(int j = 0;j <nums.size();j++){
+                sumBL = nums[i]+nums[j];
+                if(sumBL == target){
+                    if(i<j){
+                            res.push_back(i);
+                            res.push_back(j);
+                            break;
+                        }
+                        else continue;
+                    }
+            }
+        }
+      return res;
     }
 };
 // @lc code=end
