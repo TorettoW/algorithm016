7c579815d18f640e93e8b17f8f3b636989619c5e
IronBaby <qstoretto@sina.com>
2020-10-25 21:59:30 +0800

    week06

---
+++ Week_06/621.任务调度器.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=621 lang=cpp
+ *
+ * [621] 任务调度器
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+         int len=tasks.size();
+        vector<int> vec(26);
+        for(char c:tasks) ++vec[c-'A'];
+        sort(vec.begin(),vec.end(),[](int& x,int&y){return x>y;});
+        int cnt=1;
+        while(cnt<vec.size()&&vec[cnt]==vec[0]) cnt++;
+        return max(len,cnt+(n+1)*(vec[0]-1) );
+    }
+};
+// @lc code=end
+
